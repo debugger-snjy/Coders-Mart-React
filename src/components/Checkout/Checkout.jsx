8e7fd388cf6f3b50a1f8ac28ae844232a5b8e591
Sanjay Sukhwani <sanjayasukhwani@gmail.com>
2024-06-27 13:25:44 +0530

    Update : Added the User Logged in Condition | Out Of Stock in Cart Condition

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -9,26 +9,37 @@ import toast from 'react-hot-toast'
 import { placeOrderAPI } from "../../api/order.service.js"
 import { getCartItems } from '../../api/cart.service'
 import { useNavigate } from 'react-router-dom'
+import { isUserLoggedIn } from '../../utils/tokenOperations.js'
 
 function Checkout() {
 
     // Getting the card context. 
     const { state, dispatch } = useContext(cartContext)
-
+    console.log(state)
     const navigateTo = useNavigate();
-
-    if (state === null || !state) {
-        navigateTo("/")
-    }
 
     useEffect(() => {
 
-        if (state.user === null) {
-            toast.error("Kindly Login to Checkout !!")
+        if (!isUserLoggedIn()) {
+            localStorage.setItem("toastError", "Kindly Login to Checkout");
             navigateTo("/login")
+            return
+        }
+
+        let outOfStockItems = cartItems.filter((item) => item.productInStock === 0)
+
+        if (outOfStockItems.length > 0) {
+            localStorage.setItem("toastError", "Sorry, Can't Checkout As Item in Your Cart is Out Of Stock !!")
+            navigateTo("/cart")
+            return
+        }
+
+        if (state === null || !state) {
+            navigateTo("/")
         }
 
     }, [])
+
 
     const [userAddress, setUserAddress] = useState(JSON.parse(localStorage.getItem("user")) ? JSON.parse(localStorage.getItem("user")).address : "")
 
@@ -115,80 +126,83 @@ function Checkout() {
         const amount = cartItems.reduce((total, item) => total + item.productPrice * item.productQuantity, 0);
         return Math.round(amount * 1000) / 1000
     }, [cartItems]);
-
-    return (
-        <>
-            <div className="grid space-x-10 sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32 py-10 dark:bg-[#252d37] dark:text-white" style={cartItems.length < 3 ? { height: "100vh" } : {}}>
-                <div className="px-2">
-                    <p className="text-black dark:text-white text-xl font-medium">Order Summary</p>
-                    <p className="text-gray-400 dark:text-gray-300">Check your items. And select a suitable shipping method.</p>
-                    <div className="space-y-3 rounded-lg py-4">
-                        <ul role="list" className="">
-                            {state.cartItems.map((product) => (
-                                <React.Fragment className="">
-                                    <OrderItem product={product} key={product.itemID} />
-                                </React.Fragment>
-                            ))}
-                        </ul>
-                    </div>
-
-                    <p className="mt-8 text-lg font-medium">Payment Methods</p>
-                    <form className="mt-5 grid gap-6">
-                        <div className="relative">
-                            <input className="peer hidden" id="radio_1" type="radio" name="radio" checked />
-                            <span className="peer-checked:border-gray-700 dark:peer-checked:border-gray-200 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 dark:border-gray-600 "></span>
-                            <label className="peer-checked:border-2 peer-checked:border-gray-700 dark:peer-checked:border-gray-200 peer-checked:bg-gray-50 dark:peer-checked:bg-gray-700 flex cursor-pointer select-none rounded-lg border border-gray-300 dark:border-gray-600 p-4" htmlFor="radio_1">
-                                <img src={payByCheque} style={{ filter: "invert(1)" }} width={"25px"} />
-                                <div className="ml-5">
-                                    <span className="mt-2 font-semibold">Pay By Cheque On Delivery</span>
-                                </div>
-                            </label>
+    
+    if (isUserLoggedIn()) {
+        return (
+            <>
+                <div className="grid space-x-10 sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32 py-10 dark:bg-[#252d37] dark:text-white" style={cartItems.length < 3 ? { height: "100vh" } : {}}>
+                    <div className="px-2">
+                        <p className="text-black dark:text-white text-xl font-medium">Order Summary</p>
+                        <p className="text-gray-400 dark:text-gray-300">Check your items. And select a suitable shipping method.</p>
+                        <div className="space-y-3 rounded-lg py-4">
+                            <ul role="list" className="">
+                                {state.cartItems.map((product) => (
+                                    <React.Fragment className="">
+                                        <OrderItem product={product} key={product.itemID} />
+                                    </React.Fragment>
+                                ))}
+                            </ul>
                         </div>
-                        <div className="relative">
-                            <input className="peer hidden" id="radio_2" type="radio" name="radio" checked />
-                            <span className="peer-checked:border-gray-700 dark:peer-checked:border-gray-200 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 dark:border-gray-600 "></span>
-                            <label className="peer-checked:border-2 peer-checked:border-gray-700 dark:peer-checked:border-gray-200 peer-checked:bg-gray-50 dark:peer-checked:bg-gray-700 flex cursor-pointer select-none rounded-lg border border-gray-300 dark:border-gray-600 p-4" htmlFor="radio_2">
-                                <img src={payByCash} style={{ filter: "invert(1)" }} width={"25px"} />
-                                <div className="ml-5">
-                                    <span className="mt-2 font-semibold">Pay By Cash On Delivery</span>
-                                </div>
-                            </label>
-                        </div>
-                    </form>
-                </div>
-                <div className="h-fit bg-gray-50 dark:bg-gray-700 rounded-xl shadow-lg shadow-gray-900 px-4 pt-8 lg:mt-0">
-                    <p className="text-xl font-medium">Payment Details</p>
-                    <p className="text-gray-400 dark:text-gray-300">Complete your order by providing your details.</p>
-                    <div className="">
 
-                        <label htmlFor="billing-address" className="mt-4 mb-2 block text-md font-medium"> <MapPin className='inline-block' /> Billing Address <span className="text-red-700">*</span></label>
-                        <textarea id="address" rows="3" className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Add Your Address Here" value={userAddress} onChange={(e) => setUserAddress(e.target.value)}></textarea>
-                        <div className="mt-6 border-t border-b py-2">
-                            <div className="flex items-center justify-between">
-                                <p className="text-md font-medium text-gray-900 dark:text-white">Subtotal</p>
-                                <p className="font-semibold text-gray-900 dark:text-white">₹ {totalBill}</p>
+                        <p className="mt-8 text-lg font-medium">Payment Methods</p>
+                        <form className="mt-5 grid gap-6">
+                            <div className="relative">
+                                <input className="peer hidden" id="radio_1" type="radio" name="radio" checked />
+                                <span className="peer-checked:border-gray-700 dark:peer-checked:border-gray-200 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 dark:border-gray-600 "></span>
+                                <label className="peer-checked:border-2 peer-checked:border-gray-700 dark:peer-checked:border-gray-200 peer-checked:bg-gray-50 dark:peer-checked:bg-gray-700 flex cursor-pointer select-none rounded-lg border border-gray-300 dark:border-gray-600 p-4" htmlFor="radio_1">
+                                    <img src={payByCheque} style={{ filter: "invert(1)" }} width={"25px"} />
+                                    <div className="ml-5">
+                                        <span className="mt-2 font-semibold">Pay By Cheque On Delivery</span>
+                                    </div>
+                                </label>
                             </div>
-                            <div className="flex items-center justify-between">
-                                <p className="text-md font-medium text-gray-900 dark:text-white">Shipping</p>
-                                <p className="font-semibold text-gray-900 dark:text-white">₹ 1000</p>
+                            <div className="relative">
+                                <input className="peer hidden" id="radio_2" type="radio" name="radio" checked />
+                                <span className="peer-checked:border-gray-700 dark:peer-checked:border-gray-200 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 dark:border-gray-600 "></span>
+                                <label className="peer-checked:border-2 peer-checked:border-gray-700 dark:peer-checked:border-gray-200 peer-checked:bg-gray-50 dark:peer-checked:bg-gray-700 flex cursor-pointer select-none rounded-lg border border-gray-300 dark:border-gray-600 p-4" htmlFor="radio_2">
+                                    <img src={payByCash} style={{ filter: "invert(1)" }} width={"25px"} />
+                                    <div className="ml-5">
+                                        <span className="mt-2 font-semibold">Pay By Cash On Delivery</span>
+                                    </div>
+                                </label>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="h-fit bg-gray-50 dark:bg-gray-700 rounded-xl shadow-lg shadow-gray-900 px-4 pt-8 lg:mt-0">
+                        <p className="text-xl font-medium">Payment Details</p>
+                        <p className="text-gray-400 dark:text-gray-300">Complete your order by providing your details.</p>
+                        <div className="">
+
+                            <label htmlFor="billing-address" className="mt-4 mb-2 block text-md font-medium"> <MapPin className='inline-block' /> Billing Address <span className="text-red-700">*</span></label>
+                            <textarea id="address" rows="3" className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Add Your Address Here" value={userAddress} onChange={(e) => setUserAddress(e.target.value)}></textarea>
+                            <div className="mt-6 border-t border-b py-2">
+                                <div className="flex items-center justify-between">
+                                    <p className="text-md font-medium text-gray-900 dark:text-white">Subtotal</p>
+                                    <p className="font-semibold text-gray-900 dark:text-white">₹ {totalBill}</p>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <p className="text-md font-medium text-gray-900 dark:text-white">Shipping</p>
+                                    <p className="font-semibold text-gray-900 dark:text-white">₹ 1000</p>
+                                </div>
+                            </div>
+                            <div className="mt-6 flex items-center justify-between">
+                                <p className="text-xl font-semibold text-gray-900 dark:text-white">Total</p>
+                                <p className="text-xl font-semibold text-gray-900 dark:text-white">₹ {totalBill + 1000}</p>
                             </div>
                         </div>
-                        <div className="mt-6 flex items-center justify-between">
-                            <p className="text-xl font-semibold text-gray-900 dark:text-white">Total</p>
-                            <p className="text-xl font-semibold text-gray-900 dark:text-white">₹ {totalBill + 1000}</p>
+                        <div className="text-center">
+                            <button onClick={() => placeOrder()} className="mt-4 mb-8 w-3/4 rounded-md bg-black dark:bg-white dark:text-black px-6 py-3 font-extrabold text-white hover:bg-gray-700 dark:hover:bg-gray-200">
+                                <PackageCheck className='inline mr-2' height={30} width={30} />
+                                Place Order
+                            </button>
                         </div>
                     </div>
-                    <div className="text-center">
-                        <button onClick={() => placeOrder()} className="mt-4 mb-8 w-3/4 rounded-md bg-black dark:bg-white dark:text-black px-6 py-3 font-extrabold text-white hover:bg-gray-700 dark:hover:bg-gray-200">
-                            <PackageCheck className='inline mr-2' height={30} width={30} />
-                            Place Order
-                        </button>
-                    </div>
                 </div>
-            </div>
 
-        </>
-    )
+            </>
+        )
+    }
+
 }
 
 export default Checkout
